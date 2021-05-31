d28cbcfd1ba81cb2a3db95dd981f3fa660315ec8
Sam.Tek <sonny.foot@hotmail.com>
2021-05-31 17:05:29 +0200

    Generer une api pour article j ai des problèmes avec l apicontroller a revoir #2

---
+++ Wiky/Controllers/ApiArticleController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Wiky;
+using Wiky.Models;
+using Wiky.Repository;
+
+namespace Wiky.Controllers
+{
+    public class ApiArticleController : ApiController
+    {
+        private Wikydb db = new Wikydb();
+
+        // GET: api/ApiArticle
+        public IEnumerable<Article> GetArticle()
+        {
+            return new ArticleRepository().FindAllArticle();
+        }
+
+        // GET: api/ApiArticle/5
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult GetArticle(int id)
+        {
+            Article article = db.Article.Find(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(article);
+        }
+
+        // PUT: api/ApiArticle/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutArticle(int id, Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ApiArticle
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult PostArticle(Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Article.Add(article);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = article.Id }, article);
+        }
+
+        // DELETE: api/ApiArticle/5
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult DeleteArticle(int id)
+        {
+            Article article = db.Article.Find(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            db.Article.Remove(article);
+            db.SaveChanges();
+
+            return Ok(article);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return db.Article.Count(e => e.Id == id) > 0;
+        }
+    }
+}
